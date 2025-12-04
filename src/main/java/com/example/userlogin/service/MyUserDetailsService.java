32dc0736818e70587a4dcba5fcc6ba5e3ba2bc4b
vikeshsurya chava <145641308+learnfromtheskratch@users.noreply.github.com>
2025-12-04 19:36:30 +0530

    Add logging for user loading by email
    Added logging statements to track user loading process.

--- src/main/java/com/example/userlogin/service/MyUserDetailsService.java
+++ src/main/java/com/example/userlogin/service/MyUserDetailsService.java
@@ -4,7 +4,6 @@ import com.example.userlogin.Models.UserPrincipal;
 import com.example.userlogin.Models.userinfo;
 import com.example.userlogin.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -19,12 +18,11 @@ public class MyUserDetailsService implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-
+        System.out.println("Trying to load user with email: " + email);
         userinfo user = repo.findByEmail(email)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-
+        System.out.println("User found: " + user.getEmail() + ", encoded password: " + user.getPassword());
         return new UserPrincipal(user); // Assuming UserPrincipal implements UserDetails
     }
-
-
 }
+
