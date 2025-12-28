a96084978562f427bd80c8f1493b84b7f6cd1feb
vikeshsurya chava <145641308+learnfromtheskratch@users.noreply.github.com>
2025-12-29 00:34:08 +0530

    Simplify application startup configuration
    Removed dotenv configuration and set server port directly.

--- src/main/java/com/example/userlogin/UserloginApplication.java
+++ src/main/java/com/example/userlogin/UserloginApplication.java
@@ -1,21 +1,17 @@
 package com.example.userlogin;
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
 
 @SpringBootApplication
 public class UserloginApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		System.setProperty("server.port", dotenv.get("SERVER_PORT"));
-		System.setProperty("spring.datasource.url", dotenv.get("DB_URL"));
-		System.setProperty("spring.datasource.username", dotenv.get("DB_USERNAME"));
-		System.setProperty("spring.datasource.password", dotenv.get("DB_PASSWORD"));
-		System.setProperty("jwt.secret", dotenv.get("JWT_SECRET"));
-		System.setProperty("jwt.expiration", dotenv.get("JWT_EXPIRATION"));
+		System.setProperty("server.port", "8082");
 		SpringApplication.run(UserloginApplication.class, args);
 
 	}
 
 }
+
